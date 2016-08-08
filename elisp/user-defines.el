f232d390232dc9f11dda48d30fcf605357057c88
xampre <xampre@vivaldi.net>
2016-08-08 09:11:04 +0900

    add move-line-up and move-line-down function

--- elisp/user-defines.el
+++ elisp/user-defines.el
@@ -3,6 +3,17 @@
 (require 'f)
 (require 's)
 
+(defun move-line-up ()
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+
+(defun move-line-down ()
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
+
 (defun backward-kill-line (arg)
   "Kill ARG lines backward."
   (interactive "p")
