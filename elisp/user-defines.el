718c1cbb924880a62f87f9d1804defdf501bf9c6
xampre <xampre@vivaldi.net>
2018-10-09 17:27:50 +0900

    Add sort-words function

--- elisp/user-defines.el
+++ elisp/user-defines.el
@@ -4,6 +4,17 @@
 (require 's)
 (require 'fn)
 
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+    Prefixed with negative \\[universal-argument], sorts in reverse.
+
+    The variable `sort-fold-case' determines whether alphabetic case
+    affects the sort order.
+
+    See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
+
 (defun move-line-up ()
   (interactive)
   (transpose-lines 1)
